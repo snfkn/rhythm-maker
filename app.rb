e718221283a05a62d9f348961fbc8ab1390aa64d
Adam Guscott <adguscott@gmail.com>
2019-09-23 12:51:27 +0100

    V1.0

---
+++ app.rb
@@ -0,0 +1,19 @@
+require 'sinatra'
+
+def rhythm_randomizer(rhythm)
+    new_rhythm = Array.new
+    for note in rhythm do
+        unless rand(1..3).even?
+            new_rhythm.push("#{note}")
+        else
+            new_rhythm.push("<span id=\"off\">#{note}</span>")
+        end
+    end
+    return new_rhythm.join("  ")
+end
+
+get '/' do
+    @rhythm = ["1", "&", "2", "&", "3", "&", "4", "&"]
+    @rhythm = erb rhythm_randomizer(@rhythm)
+    erb :index, :layout => :template
+end
